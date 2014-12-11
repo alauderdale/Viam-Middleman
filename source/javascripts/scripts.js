6a4b66d8c705110fb90f3c54b2f31a6a7eb57976
Alex Lauderdale <alauderdale@mac.com>
2014-12-10 22:43:47 -0700

    menu

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -12,5 +12,13 @@ $(document).ready(function(){
 
 	$('.tooltip-up').tooltip({html:true});
 
+	$( "#js-menu-button" ).click(function() {
+    $( ".main-nav-container" ).toggleClass( "fixed-nav" );
+    $( ".menu-drop" ).toggleClass( "menu-show" );
+    $( "html" ).toggleClass( "nav-open" );
+    $( "#js-menu-button i" ).toggleClass( "fa-navicon fa-times" );
+
+  });
+
 
 });
